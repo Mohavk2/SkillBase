25521ba43daf5c133ebe2fbdc08e48d40733c6fb
Mohavk <mohavkv12@gmail.com>
2022-10-16 14:09:06 +0200

    Change. Skills design

---
+++ Views/Skills/SkillCardFaceUC.xaml.cs
@@ -0,0 +1,42 @@
+﻿using SkillBase.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SkillBase.Views.Skills
+{
+    /// <summary>
+    /// Interaction logic for SkillCardFaceUC.xaml
+    /// </summary>
+    public partial class SkillCardFaceUC : UserControl
+    {
+        public SkillCardFaceUC()
+        {
+            InitializeComponent();
+        }
+
+        private void AddSkill_Click(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void Header_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(this, new DataObject(DataFormats.Serializable, this), DragDropEffects.Move);
+            }
+        }
+    }
+}
--- Views/Skills/SkillUC.xaml.cs
+++ Views/Skills/SkillUC.xaml.cs
@@ -17,7 +17,7 @@ namespace SkillBase.Views.Skills
     public partial class SkillUC : UserControl
     {
         public static readonly DependencyProperty SkillDropCommandProperty = DependencyProperty
-            .Register("SkillDropCommand", typeof(ICommand), typeof(SkillUC), new PropertyMetadata(null));
+.Register("SkillDropCommand", typeof(ICommand), typeof(SkillUC), new PropertyMetadata(null));
 
         public ICommand SkillDropCommand
         {
@@ -30,109 +30,16 @@ namespace SkillBase.Views.Skills
             InitializeComponent();
         }
 
-        private void AddSkill_Click(object sender, RoutedEventArgs e)
-        {
-            ShowSubskillsToggle.IsChecked = true;
-            e.Handled = true;
-        }
-
-        private void UserControl_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            var skillName = sender as TextBox;
-            if (skillName != null && skillName.Name == "SkillName" && skillName.IsFocused)
-            {
-                skillName.IsEnabled = true;
-            }
-        }
-
-        private void EditNameToggle_Click(object sender, RoutedEventArgs e)
-        {
-            if (EditNameToggle.IsChecked == true)
-            {
-                SkillName.IsReadOnly = false;
-                SkillName.Focus();
-                SkillName.CaretIndex = SkillName.Text.Length;
-                SkillName.CaretBrush = null;
-            }
-            else
-            {
-                SkillName.IsReadOnly = true;
-            }
-            e.Handled = true;
-        }
-
-        private void SkillName_LostFocus(object sender, RoutedEventArgs e)
-        {
-            EditNameToggle.IsChecked = false;
-            SkillName.IsReadOnly = true;
-            SkillName.CaretBrush = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
-        }
-
-        private void SkillName_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Return)
-            {
-                EditNameToggle.IsChecked = false;
-                SkillName.IsReadOnly = true;
-                SkillName.CaretBrush = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
-            }
-        }
-
-        private void SkillDescription_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(SkillDescription.Text))
-            {
-                SkillDescription.Text = "Description...";
-            }
-        }
-
-        private void SkillDescription_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (SkillDescription.Text == "Description...")
-            {
-                SkillDescription.Text = "";
-            }
-        }
 
         private void AddDayTask_Click(object sender, RoutedEventArgs e)
         {
             TasksExpander.IsExpanded = true;
         }
 
-        private void Notes_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (Notes.Text == "Write your notes here...")
-            {
-                Notes.Text = "";
-            }
-        }
-
-        private void Notes_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(Notes.Text))
-            {
-                Notes.Text = "Write your notes here...";
-            }
-        }
-
-        private void CardFooter_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            SkillCard.Visibility = SkillCard.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
-            CollapsIcon.Kind = CollapsIcon.Kind == PackIconKind.ArrowCollapseDown ? PackIconKind.ArrowCollapseUp : PackIconKind.ArrowCollapseDown;
-        }
-
-        private void Button_PreviewMouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                DragDrop.DoDragDrop(this, new DataObject(DataFormats.Serializable, this), DragDropEffects.Move);
-            }
-        }
-
         private void UserControl_Drop(object sender, DragEventArgs e)
         {
             e.Handled = true;
-            var uc = e.Data.GetData(DataFormats.Serializable) as SkillUC;
+            var uc = e.Data.GetData(DataFormats.Serializable) as SkillCardFaceUC;
             var vm = uc?.DataContext as SkillViewModel;
 
             if (vm != null)
